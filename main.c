a69b9d533e2cc1c68ae63521ca5e067e3d27010e
u know who i am <uknow@whoiam.com>
2019-09-12 13:47:37 +0200

    rien

--- main.c
+++ main.c
@@ -16,16 +16,6 @@ int main(void)
 	MCUCR |= 1 << SRE; // enable external memory
 	SFIOR |= 1 << XMM2; // disable flashing pins (we only use 12 pins for addressing)
 
-	//Test connection to ADC with the GAL
-	volatile char *ext_ram = (char *) 0x1800;
-	while(1)
-	{
-		ext_ram[0x1400] = 1;
-		_delay_ms(1000);
-		ext_ram[0x1400] = 0;
-		_delay_ms(1000);
-	}
-	
 	//Adressing SRAM test
 	//while (1) SRAM_test();
 
