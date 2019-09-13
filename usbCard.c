56ac72edc64d2f34c040e0784d65f441822352d6
mariusvh <mariusvh@stud.ntnu.no>
2019-09-13 14:26:59 +0200

    nothing more

---
+++ usbCard.c
@@ -0,0 +1,16 @@
+#define DIR_Y 0x7
+#define DIR_X 0x6
+#define LEFT_SLIDER 0x5
+#define RIGHT_SLIDER 0x4
+
+uint8_t getJoystick(uint8_t direction)
+{
+	volatile char *ext_adc = (char *) 0x1400;
+	
+	ext_adc[0] = direction;
+	_delay_ms(20) ;
+	uint8_t result = ext_adc[0];
+	return result;
+//	printf("%d\n", result);
+}
+
